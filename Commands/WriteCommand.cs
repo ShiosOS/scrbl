f7af74986a21afefaa74c8074d2c1985f30174a8
Julius Walton <juliuswalton7901@gmail.com>
2025-07-06 03:42:47 -0400

    add write command boiler plate

---
+++ Commands/WriteCommand.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console.Cli;
+
+namespace scrbl.Commands;
+
+public class WriteCommand : Command<CreateCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        
+    }
+
+    public override int Execute(CommandContext context, CreateCommand.Settings settings)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,8 +15,12 @@ internal static class Program
             
             config.AddCommand<CreateCommand>("create")
                 .WithDescription("Create a templated section")
-                .WithExample(new[] { "create", "-d" })
-                .WithExample(new[] { "create", "--template", "daily" });
+                .WithExample("create", "-d")
+                .WithExample("create", "--template", "daily");
+
+            config.AddCommand<WriteCommand>("write")
+                .WithDescription("Write to the notes file")
+                .WithExample("write", "-w");
         });
         
         return app.Run(args);
